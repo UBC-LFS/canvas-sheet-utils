5ff49b8a07f94c362d571aa97de1b91a289cd684
KevinCHWu <kevinwu0703@gmail.com>
2018-06-07 15:55:30 -0700

    TestFunct currently format the retrieved info for writing to sheets

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const fs = require('fs')
 const readline = require('readline')
-const {google} = require('googleapis')
+const { google } = require('googleapis')
 
 // If modifying these scopes, delete credentials.json.
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 const TOKEN_PATH = 'credentials.json'
 
 // Load client secrets from a local file.
-const editFile = (actionFunct, data) => {
+const editFile = (actionFunct) => {
   fs.readFile('client_secret.json', (err, content) => {
     if (err) return console.log('Error loading client secret file:', err)
     // Authorize a client with credentials, then call the Google Sheets API.
-    authorize(JSON.parse(content), actionFunct, data)
+    authorize(JSON.parse(content), actionFunct)
   })
 }
 
@@ -21,8 +21,8 @@ const editFile = (actionFunct, data) => {
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize (credentials, callback, data) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed
+function authorize(credentials, callback) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed
   const oAuth2Client = new google.auth.OAuth2(
     client_id, client_secret, redirect_uris[0])
 
@@ -30,7 +30,7 @@ function authorize (credentials, callback, data) {
   fs.readFile(TOKEN_PATH, (err, token) => {
     if (err) return getNewToken(oAuth2Client, callback)
     oAuth2Client.setCredentials(JSON.parse(token))
-    callback(oAuth2Client, data)
+    callback(oAuth2Client)
   })
 }
 
@@ -40,7 +40,7 @@ function authorize (credentials, callback, data) {
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
-function getNewToken (oAuth2Client, callback) {
+function getNewToken(oAuth2Client, callback) {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
@@ -70,12 +70,12 @@ function getNewToken (oAuth2Client, callback) {
  * @see https://docs.google.com/spreadsheets/d/1Df4TF67exj3_6H9B53sIp5lbRS_3FBt011sGRJ-zVAg/edit#gid=2097143836
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
-function listCourses (auth, range = '') {
-  const sheets = google.sheets({version: 'v4', auth})
+function listCourses(auth) {
+  const sheets = google.sheets({ version: 'v4', auth })
   sheets.spreadsheets.values.get({
     spreadsheetId: '1Df4TF67exj3_6H9B53sIp5lbRS_3FBt011sGRJ-zVAg',
     range: '2018W1!A2:N',
-  }, (err, {data}) => {
+  }, (err, { data }) => {
     if (err) return console.log('The API returned an error: ' + err)
     const rows = data.values
     if (rows.length) {
@@ -91,12 +91,12 @@ function listCourses (auth, range = '') {
   })
 }
 
-function listCanvasCourseCopies (auth, range = '') {
-  const sheets = google.sheets({version: 'v4', auth})
+function listCanvasCourseCopies(auth) {
+  const sheets = google.sheets({ version: 'v4', auth })
   sheets.spreadsheets.values.get({
     spreadsheetId: '1Df4TF67exj3_6H9B53sIp5lbRS_3FBt011sGRJ-zVAg',
     range: '2018W1!A2:N',
-  }, (err, {data}) => {
+  }, (err, { data }) => {
     if (err) return console.log('The API returned an error: ' + err)
     const rows = data.values
     if (rows.length) {
@@ -112,12 +112,12 @@ function listCanvasCourseCopies (auth, range = '') {
   })
 }
 
-function listXlisted (auth, range = '') {
-  const sheets = google.sheets({version: 'v4', auth})
+function listXlisted(auth) {
+  const sheets = google.sheets({ version: 'v4', auth })
   sheets.spreadsheets.values.get({
     spreadsheetId: '1Df4TF67exj3_6H9B53sIp5lbRS_3FBt011sGRJ-zVAg',
     range: '2018W1!A2:N',
-  }, (err, {data}) => {
+  }, (err, { data }) => {
     if (err) return console.log('The API returned an error: ' + err)
     const rows = data.values
     if (rows.length) {
@@ -133,29 +133,54 @@ function listXlisted (auth, range = '') {
   })
 }
 
-function testFunct (auth, outputArray) {
-  const sheets = google.sheets({version: 'v4', auth})
+function testFunct(auth) {
+  const sheet1 = []
+  const sheet2 = []
+  const sheets = google.sheets({ version: 'v4', auth })
   sheets.spreadsheets.values.get({
     spreadsheetId: '1Df4TF67exj3_6H9B53sIp5lbRS_3FBt011sGRJ-zVAg',
-    range: '2018W1!A2:N',
-  }, (err, {data}) => {
-    if (err) return console.log('The API returned an error: ' + err)
+    range: '2018W1!A2:N'
+  }, (err, { data }) => {
+    if (err) console.log(err)
     const rows = data.values
     if (rows.length) {
-      rows.map((row) => {
+      rows.forEach((row) => {
         if (row[13] === 'Y') {
-          outputArray.push(`${row[2]}, ${row[3]}, ${row[5]}, ${row[8]}`)
+          sheet1.push([row[2], row[3], row[5], row[7], row[6], row[8]])
         }
       })
-      console.log('Copied')
-    } else {
-      console.log('No data found.')
     }
+    sheet1.map(rows => sheet2.push({
+      courses: rows[0] + ' ' + rows[1],
+      instructor: rows[2],
+      firstName: rows[3],
+      surname: rows[4],
+      email: rows[5]}))
+    const newSheet = sheet2.reduce((acc, cur) => {
+      const index = acc.findIndex(rows => rows.instructor === cur.instructor)
+      if (index > -1) {
+        acc[index].courses += ', ' + cur.courses
+      } else {
+        acc.push(cur)
+      }
+      return acc
+    }, [])
+    const newSheetArr = newSheet.map(object => Object.values(object))
+    sheets.spreadsheets.values.get({
+      spreadsheetId: '1Df4TF67exj3_6H9B53sIp5lbRS_3FBt011sGRJ-zVAg',
+      range: 'Instructor Contact!A2:N'
+    }, (err, { data }) => {
+      if (err) console.log(err)
+      const rows = data.values
+      if (rows.length) {
+        // rows.forEach((row) => sheet2.push([row[0], row[1], row[2], row[3]]))
+      }
+      // do something here
+      console.log(newSheetArr)
+    })
   })
 }
 
 // editFile(listCanvasCourseCopies)
 
-const temp = []
-
-editFile(testFunct, temp)
+editFile(testFunct)
