b3d34f356c273435ae47ecd8af886c1129739189
KevinCHWu <kevinwu0703@gmail.com>
2018-06-14 16:04:32 -0700

    Completed most of the prompts

--- index.js
+++ index.js
@@ -8,15 +8,30 @@ const prompts = require('prompts')
 
 async function initPrompt () {
   const response = await prompts({
-    type: 'number',
-    name: 'value',
-    message: 'How old are you?'
+    type: 'text',
+    name: 'meaning',
+    message: `What would you like to do today? (type the following commands to do any actions) 
+    get courses: gets all lecture courses that are not on Canvas nor X listed
+    get courses on Canvas: gets all courses that are empty shells on Canvas
+    get X listed courses: gets all courses that require X listing
+    print to spreadsheet: prints canvas shell courses onto the google sheet; https://docs.google.com/spreadsheets/d/1hWQFrYRV745PYkznCqIPRVtD5z7faapKvkelf7ukkMw/edit#gid=0`
   })
 
-  if (response.value === 1) {
-    console.log('You are obviously not 1 year old, you liar liar pants on fire')
-  } else {
-    console.log(response.value + ' years old. WOW you are old!')
+  switch (response.meaning) {
+    case 'get courses':
+      editFile(listCourses)
+      break
+    case 'get courses on Canvas':
+      editFile(listCanvasCourseCopies)
+      break
+    case 'get X listed courses':
+      editFile(listXlisted)
+      break
+    case 'print to spreadsheet':
+      editFile(printToPage)
+      break
+    default:
+      console.log(response.meaning + '? Unable to understand. Please type again (commands are case sensitive)')
   }
 }
 
