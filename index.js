f5b62ae6ef583b0cdae48ab2946b8037f8568bdf
KevinCHWu <kevinwu0703@gmail.com>
2018-06-14 15:27:04 -0700

    Started working on prompts

--- index.js
+++ index.js
@@ -4,5 +4,20 @@ const listCanvasCourseCopies = require('./src/recipes/listCanvasCourseCopies')
 const listXlisted = require('./src/recipes/listXlisted')
 const printToPage = require('./src/recipes/printToPage')
 const listNonEmailResponse = require('./src/recipes/listNonEmailResponse')
+const prompts = require('prompts')
 
-editFile(listNonEmailResponse)
+async function initPrompt () {
+  const response = await prompts({
+    type: 'number',
+    name: 'value',
+    message: 'How old are you?'
+  })
+
+  if (response.value === 1) {
+    console.log('You are obviously not 1 year old, you liar liar pants on fire')
+  } else {
+    console.log(response.value + ' years old. WOW you are old!')
+  }
+}
+
+initPrompt()
